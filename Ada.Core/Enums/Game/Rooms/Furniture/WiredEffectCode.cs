227b34cea9a6951a99f927fa1e66bb7854666137
habtard <habtard@gmail.com>
2026-07-22 22:26:10 +0100

    add wired interaction types, condition codes and int parameter helpers

--- Ada.Core/Enums/Game/Rooms/Furniture/WiredEffectCode.cs
+++ Ada.Core/Enums/Game/Rooms/Furniture/WiredEffectCode.cs
@@ -4,10 +4,13 @@ public enum WiredEffectCode
 {
     ToggleFurnitureState = 0,
     TimerReset = 1,
+    MoveRotateFurniture = 4,
     ShowMessage = 7,
     TeleportToFurniture = 8,
     MoveFurnitureToClosestUser = 11,
+    FleeFromClosestUser = 12,
     ChangeFurnitureDirection = 13,
+    CallAnotherStack = 18,
     KickUser = 19,
     MuteTriggerer = 20,
     BotChangedClothes = 26,
