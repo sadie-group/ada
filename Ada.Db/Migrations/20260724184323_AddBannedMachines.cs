9b08732415e3953682fb0e98a3e827d6559d7627
habtard <habtard@gmail.com>
2026-07-24 19:45:49 +0100

    Add banned_machines migration

---
+++ Ada.Db/Migrations/20260724184323_AddBannedMachines.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ada.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannedMachines : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "banned_machines",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    creator_id = table.Column<long>(type: "bigint", nullable: false),
+                    reason = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    machine_id = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    expires_at = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_banned_machines", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_banned_machines_players_creator_id",
+                        column: x => x.creator_id,
+                        principalTable: "players",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_banned_machines_creator_id",
+                table: "banned_machines",
+                column: "creator_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "banned_machines");
+        }
+    }
+}
