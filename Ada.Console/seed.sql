6b419e699db3b940532a10a2a366c7162cff6d10
habtard <habtard@gmail.com>
2026-07-31 21:54:25 +0100

    navigator seed record

--- Ada.Console/seed.sql
+++ Ada.Console/seed.sql
@@ -77371,7 +77371,8 @@ INSERT INTO `navigator_categories` (
     `id`, `name`, `code_name`, `tab_id`, `order_id`
 ) VALUES
 (1, 'Most Popular Rooms', 'popular', 3, 0),
-(2, 'My Rooms', 'my_rooms', 4, 0);
+(2, 'My Rooms', 'my_rooms', 4, 0),
+(3, 'Public Rooms', 'official', 2, 0);
 INSERT INTO `roles_permissions` (`role_id`, `permission_id`) VALUES
 (6, 1),
 (6, 2),
