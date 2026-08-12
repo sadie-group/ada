db33db0b12c8e7b71ca47fba3c45cbbb08285463
habtard <habtard@gmail.com>
2026-08-12 15:11:39 +0100

    search limits

--- Ada.Core/Shared/Constants/SearchLimits.cs
+++ Ada.Core/Shared/Constants/SearchLimits.cs
@@ -3,6 +3,6 @@ namespace Ada.Core.Shared.Constants;
 public static class SearchLimits
 {
     public const int MinPlayerQueryLength = 3;
-
     public const int MaxPlayerResults = 50;
+    public const int NavigatorTags = 50;
 }
