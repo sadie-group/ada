db1c094e7203410375af36513446efa55044159b
habtard <habtard@gmail.com>
2026-08-12 15:06:16 +0100

    perf improve

--- Ada.Networking/Client/ClientDisposalService.cs
+++ Ada.Networking/Client/ClientDisposalService.cs
@@ -1,11 +1,23 @@
+using Ada.API.Interfaces.Game.Guides;
 using Ada.API.Interfaces.Networking.Client;
 
 namespace Ada.Networking.Client;
 
-public class ClientDisposalService(INetworkClientRepository clientRepository) : IClientDisposalService
+public class ClientDisposalService(
+    INetworkClientRepository clientRepository,
+    IWalkRequestThrottle walkThrottle,
+    IGuideSessionService guideSessionService) : IClientDisposalService
 {
     public async Task HandleDisconnectAsync(INetworkClient client)
     {
+        var playerId = client.Player?.Player.Id;
+
+        if (playerId.HasValue)
+        {
+            walkThrottle.Forget(playerId.Value);
+            guideSessionService.Forget(playerId.Value);
+        }
+
         await clientRepository.TryRemoveAsync(client.Guid);
         await client.DisposeAsync();
     }
