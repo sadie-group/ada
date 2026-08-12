355721c850c5070f7deb954a8db747719a6a713d
habtard <habtard@gmail.com>
2026-08-12 15:07:06 +0100

    guide session bits

---
+++ Ada.API/Interfaces/Game/Guides/GuideSessionEndReason.cs
@@ -0,0 +1,10 @@
+
+namespace Ada.API.Interfaces.Game.Guides;
+
+public enum GuideSessionEndReason
+{
+    HelperEnded = 0,
+    RequesterCancelled = 1,
+    HelperDisconnected = 2,
+    RequesterDisconnected = 3
+}
